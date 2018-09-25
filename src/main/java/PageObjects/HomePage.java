bb135ce017bb58ce2e194b613ee0bd61de4f4861
ramyajayaram <36662300+ramyajayaram@users.noreply.github.com>
2018-09-25 17:16:57 +0530

    changes to Tests

--- src/main/java/PageObjects/HomePage.java
+++ src/main/java/PageObjects/HomePage.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -19,7 +20,8 @@ public class HomePage {
 	{
 		PageFactory.initElements(PropertyCollection.driver,this);
 	}	
-	
+ @FindBy(xpath="//span[contains(text(),'Search by Hotel or City Name')]")	
+ private static WebElement AutoSuggest;
 
 	@FindBy(xpath = "//div[@id='select2-drop']//input[@type='text']")
 	private static WebElement HotelsAutoSuggestTxtBox;
@@ -30,13 +32,23 @@ public class HomePage {
 	@FindBy(xpath="//div[@class='select2-drop select2-display-none select2-with-searchbox select2-drop-active']//ul[@class='select2-results']//ul[@class='select2-result-sub']//li[1]")
 	private static WebElement autoSuggestion;
 	
-	public void HotelsSearch() throws InterruptedException
+	@FindBy(xpath = "//div[@id='s2id_autogen8']//a[@class='select2-choice']")
+	private static WebElement HotelsTxtBox;
+	
+	
+	public void HotelsSearch() throws InterruptedException 
 	{
-		Thread.sleep(5000);
-		HotelsAutoSuggestTxtBox.sendKeys(SearchData.HotelName);
+		
+		Thread.sleep(50000);
+		 Actions action = new Actions(PropertyCollection.driver);
+		 WebElement element =PropertyCollection. driver.findElement(By.xpath("//div[@id='s2id_autogen8']//a[@class='select2-choice']"));
+
+	    action.moveToElement(element).build().perform();
+	    element.click();
+		HotelsAutoSuggestTxtBox.sendKeys("Tas");
 		HotelsAutoSuggestTxtBox.sendKeys(Keys.ENTER);
 		WebDriverWait wait = new WebDriverWait(PropertyCollection.driver, 10);
-		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='select2-drop select2-display-none select2-with-searchbox select2-drop-active']//ul[@class='select2-results']//ul[@class='select2-result-sub']//li[1]")));
+		WebElement element1 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='select2-drop select2-display-none select2-with-searchbox select2-drop-active']//ul[@class='select2-results']//ul[@class='select2-result-sub']//li[1]")));
 		autoSuggestion.click();
 		 
 		
